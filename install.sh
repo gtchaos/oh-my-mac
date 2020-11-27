63fd50b0e20a8ab49f1763cad5bbca048e8f8f0d
gaomiaomiao <gaomiaomiao@baidu.com>
2020-11-27 16:28:38 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,14 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+brew install autojump
+
 mv ~/.vimrc ~/.vimrc.backup
 cp vimrc ~/.vimrc
 mv ~/.zshrc ~/.zshrc.backup
 cp zshrc ~/.zshrc
 mv ~/.bash_profile ~/.bash_profile.backup
 cp bash_profile ~/.bash_profile
-mv ~/.bash_aliases ~/.bash_aliases.backup
-cp bash_aliases ~/.bash_aliases 
 source ~/.zshrc
