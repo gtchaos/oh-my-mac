5914c4e0899d94c11ce847c43d4137d23a47de0d
gtchaos <gsls1817@gmail.com>
2022-04-05 14:51:52 +0800

    add homebrew

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-# clone git repo bundle
+# install homebrew
+
+/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+
+# install bundle
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 tar zxf bundle.tar.gz 
 cp -rf bundle ~/.vim
@@ -8,12 +12,13 @@ cp -rf bundle ~/.vim
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# install autojump, autosuggestions and syntax-highlighting
+brew install autojump
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# backup 
 source bash_profile
-
-brew install autojump
 
 mv ~/.vimrc ~/.vimrc.backup
 
